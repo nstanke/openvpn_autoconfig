61fe3f656b0baf5ba36449eda5e5af97b2aee2c0
Norman <norman@nstanke.at>
2015-02-22 21:33:17 +0100

    IPv6 over IPv4

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
-openvpn_autoconfig
+openvpn_autoconfig - IPv6 over IPv&
 ==================
 
-Configuration scripts to automatically configure OpenVPN on an Ubuntu server.
+Configuration scripts to automatically configure a OpenVPN IPv6 broker on an Ubuntu server based on https://github.com/tinfoil/openvpn_autoconfig.
+
 
 Inspired/borrowed heavily from https://github.com/jpetazzo/dockvpn and https://www.tinfoilsecurity.com/blog/dont-get-pwned-on-public-wifi-use-your-own-vpn-tutorial-guide-how-to.
--- bin/openvpn.sh
+++ bin/openvpn.sh
@@ -42,10 +42,13 @@ chmod 600 *-key.pem
 
 # Set up IP forwarding and NAT for iptables
 >>/etc/sysctl.conf echo net.ipv4.ip_forward=1
+>>/etc/sysctl.conf echo net.ipv6.conf.all.forwarding=1
 sysctl -p
 
 iptables -t nat -A POSTROUTING -s 10.8.0.0/24 -o eth0 -j MASQUERADE
 >/etc/iptables/rules.v4 iptables-save
+ip6tables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
+>/etc/iptables/ip6tables.rules ip6tables-save 
 
 # Write configuration files for client and server
 
@@ -53,6 +56,7 @@ SERVER_IP=$(curl -s4 https://canhazip.com || echo "<insert server IP here>")
 
 >tcp443.conf cat <<EOF
 server      10.8.0.0 255.255.255.0
+server-ipv6 2a02:3456:ef01:abcd:2::/80
 verb        3
 duplicate-cn
 key         server-key.pem
@@ -67,7 +71,7 @@ push        "dhcp-option DNS 8.8.8.8"
 push        "dhcp-option DNS 8.8.4.4"
 
 # Normally, the following command is sufficient.
-# However, it doesn't assign a gateway when using 
+# However, it doesn't assign a gateway when using
 # VMware guest-only networking.
 #
 # push        "redirect-gateway def1 bypass-dhcp"
@@ -75,6 +79,7 @@ push        "dhcp-option DNS 8.8.4.4"
 push        "redirect-gateway bypass-dhcp"
 push        "route-metric 512"
 push        "route 0.0.0.0 0.0.0.0"
+push        "route-ipv6 2000::/3"
 
 user        nobody
 group       nogroup
@@ -89,6 +94,7 @@ EOF
 client
 nobind
 dev tun
+tun-ipv6
 redirect-gateway def1 bypass-dhcp
 remote $SERVER_IP 443 tcp
 comp-lzo yes
